f5e1affc3b2808471920d2abd537c972433846a6
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-08-19 16:16:59 +0530

    All the loadtesting script added for all the page

---
+++ ASME-India/Performance/test/config.js
@@ -0,0 +1,9 @@
+// config.js
+export const config = {
+  baseUrl: 'https://www.asme-india.org',
+  contactUrl: 'https://www.asme-india.org/imece/contact-us',
+  targetUsers: 200,
+  rampUpDuration: '5s',
+  testDuration: '10s',
+  rampDownDuration: '5s'
+};
